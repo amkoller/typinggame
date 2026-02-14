8ed753b25d6efbf5903c88e17a1db98b38460fbc
Aaron Koller <kolleraa@gmail.com>
2026-02-13 17:07:30 -0800

    prod

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    target: "es2022",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          pixijs: ["pixi.js"],
+        },
+      },
+    },
+  },
+});
